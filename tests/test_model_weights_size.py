284aea3a6b290bad318a462f4b8f6b77ba28279b
Milan Chandra <milanchndr@gmail.com>
2025-06-29 15:23:04 +0000

    Add test for model file existence and size

---
+++ tests/test_model_weights_size.py
@@ -0,0 +1,8 @@
+import os
+
+def test_model_weights_not_empty():
+    path = "model.weights.h5"
+    assert os.path.exists(path), f"{path} does not exist"
+    size = os.path.getsize(path)
+    assert size > 1000, f"{path} file is too small ({size} bytes)"
+                    
